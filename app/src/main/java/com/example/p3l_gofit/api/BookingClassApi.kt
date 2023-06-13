aeea2ccaef3f1b2b3431cd6f82625118129dda6a
PradnyaKuswara <pradnyakuswara24@gmail.com>
2023-06-14 03:08:50 +0700

    api update

--- app/src/main/java/com/example/p3l_gofit/api/BookingClassApi.kt
+++ app/src/main/java/com/example/p3l_gofit/api/BookingClassApi.kt
@@ -2,13 +2,7 @@ package com.example.p3l_gofit.api
 
 class BookingClassApi {
     companion object {
-        //kos
-//        val BASE_URL = "http://192.168.0.106/p3l_GOFIT/public/api/"
-        //lux coffe
-//        val BASE_URL = "http://192.168.1.13/p3l_GOFIT/public/api/"
-//        val BASE_URL = "http://10.0.1.185/p3l_GOFIT/public/api/"
-        //link api diganti sesuai kebutuhan
-        val BASE_URL = "https://www.gofitsport.my.id/api/"
+        val BASE_URL = "https://gofit-sport.000webhostapp.com/api/"
 
         val GETDATASCHEDULE = BASE_URL + "data-daily-schedule/"
         val STOREDATA = BASE_URL + "booking-class"
--- app/src/main/java/com/example/p3l_gofit/api/BookingGymApi.kt
+++ app/src/main/java/com/example/p3l_gofit/api/BookingGymApi.kt
@@ -2,15 +2,8 @@ package com.example.p3l_gofit.api
 
 class BookingGymApi {
     companion object {
-        //kos
-//        val BASE_URL = "http://192.168.0.106/p3l_GOFIT/public/api/"
-        //lux coffee
-//        val BASE_URL = "http://192.168.1.13/p3l_GOFIT/public/api/"
-        //konrite
-//        val BASE_URL = "http://10.0.1.185/p3l_GOFIT/public/api/"
-
         //link api diganti sesuai kebutuhan
-        val BASE_URL = "https://www.gofitsport.my.id/api/"
+        val BASE_URL = "https://gofit-sport.000webhostapp.com/api/"
         val GETDATAHISTORY = BASE_URL + "booking-gym-history"
         val STOREDATA = BASE_URL + "booking-gym"
         val CANCELBOOKING = BASE_URL + "cancel-booking-gym/"
